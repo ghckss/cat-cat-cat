4164b744777fc423c8d9f25d934c490ec47a868b
HwangHoChan <ghckss93@gmail.com>
2021-06-06 17:08:11 +0900

    Board 컴포넌트 레이아웃 구성

--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -1,7 +1,48 @@
 import * as React from 'react';
 
+import {Container, Row} from './styles';
+import Image from 'components/Image';
+
+import cat1 from 'assets/cats/cat1.jpg';
+
 export interface IBoardProps {}
 
 export default function Board(props: IBoardProps) {
-    return <div></div>;
+    return (
+        <Container>
+            <Row>
+                <div className='img'>
+                    <Image data-src={cat1} />
+                </div>
+                <div className='img'>
+                    <Image data-src={cat1} />
+                </div>
+                <div className='img'>
+                    <Image data-src={cat1} />
+                </div>
+            </Row>
+            <Row>
+                <div className='img'>
+                    <Image data-src={cat1} />
+                </div>
+                <div className='img'>
+                    <Image data-src={cat1} />
+                </div>
+                <div className='img'>
+                    <Image data-src={cat1} />
+                </div>
+            </Row>
+            <Row>
+                <div className='img'>
+                    <Image data-src={cat1} />
+                </div>
+                <div className='img'>
+                    <Image data-src={cat1} />
+                </div>
+                <div className='img'>
+                    <Image data-src={cat1} />
+                </div>
+            </Row>
+        </Container>
+    );
 }
